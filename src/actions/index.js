99a972fb0bf354bd0e79f41d854421e41aace727
Danil Tolstov <dfrontman@gmail.com>
2022-04-28 11:29:25 +0500

    Initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+import {createAction} from "@reduxjs/toolkit";
+import {heroesFetching,heroesFetched,heroesFetchingError} from "../components/heroesList/heroesSlice";
+
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()))
+}
+
+export const fetchFilters = (request) => (dispatch) => {
+    dispatch(filtersFetching());
+    request("http://localhost:3001/filters")
+        .then(data => dispatch(filtersFetched(data)))
+        .catch(() => dispatch(filtersFetchingError()))
+}
+
+export const filtersFetching = createAction('FILTERS_FETCHING');
+
+export const filtersFetched = createAction('FILTERS_FETCHED')
+
+export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR');
+
+export const activeFilterChanged = createAction('ACTIVE_FILTER_CHANGED')
+
+
